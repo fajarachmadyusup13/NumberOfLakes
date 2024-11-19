7b685d9db36d32201569ba3ff23184b778df8638
fajarachmadyusup13 <fajarachmady13@gmail.com>
2024-11-19 13:37:48 +0700

    add interactive input functionality, and implement logging

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"github.com/fajarachmadyusup13/NumberOfLakes/utils"
+	"github.com/sirupsen/logrus"
+	"os"
+)
 
 func main() {
-	grid := [][]string{
-		{"#", ".", "#", "#", "#"},
-		{".", ".", "#", "#", "#"},
-		{"#", "#", ".", "#", "#"},
-		{"#", "#", "#", "#", "."},
+
+	reader := bufio.NewReader(os.Stdin)
+
+	width, err := utils.GetUserInputInt(reader)
+	if err != nil {
+		logrus.Error(err)
+		return
 	}
+
+	height, err := utils.GetUserInputInt(reader)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+
+	grid, err := utils.GetUserInputGrid(reader, height, width)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+
 	fmt.Printf("TOTAL: %d", numOfLakes(grid))
 }
 
+// numOfLakes counter for lakes number
 func numOfLakes(grid [][]string) int {
-
 	totalLakes := 0
 	for i := range grid {
 		for j := range grid[i] {
@@ -23,15 +44,17 @@ func numOfLakes(grid [][]string) int {
 			}
 		}
 	}
-
 	return totalLakes
 }
 
+// neighbourCheck for checking neighbour of each node, and it's done recursively
 func neighbourCheck(i, j int, grid [][]string) {
 	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] != "." {
 		return
 	}
+	// if a node was visited, remark it as land or change it from '.' to '#', so it would be not visited again on future iteration
 	grid[i][j] = "#"
+
 	neighbourCheck(i+1, j, grid)   // bottom neighbour
 	neighbourCheck(i-1, j, grid)   // top neighbour
 	neighbourCheck(i, j+1, grid)   // right neighbour
